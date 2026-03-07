485e388f9b0072123d17fcb8dd0a2823b15b0cf9
kazij317-code <kazij317@gmail.com>
2026-03-08 00:10:29 +0600

    first commit

---
+++ script_app.js
@@ -0,0 +1,200 @@
+// Count Issues:
+
+function countIssues(issues){
+
+    // total count
+    const total = issues.length;
+
+    // open count
+    const open = issues.filter(issue => issue.status === "open").length;
+
+    // closed count
+    const closed = issues.filter(issue => issue.status === "closed").length;
+
+    // update UI
+    document.getElementById("tabCount").innerText = total;
+    document.getElementById("openCount").innerText = open;
+    document.getElementById("closedCount").innerText = closed;
+}
+
+
+// Load All Issues:
+
+const API = "https://phi-lab-server.vercel.app/api/v1/lab/issues"
+
+const container = document.getElementById("issuesContainer")
+const spinner = document.getElementById("spinner")
+
+
+// Load Issues
+
+async function loadIssues(type){
+
+spinner.classList.remove("hidden")
+
+const res = await fetch(API)
+const data = await res.json()
+
+let issues = data.data
+
+
+if(type === "open"){
+issues = issues.filter(issue => issue.status === "open")
+}
+
+if(type === "closed"){
+issues = issues.filter(issue => issue.status === "closed")
+}
+countIssues(issues);
+displayIssues(issues)
+
+spinner.classList.add("hidden")
+
+}
+
+loadIssues("all")
+
+
+const createElements = (arr) =>{
+    const htmlElements = arr.map((el) => `<span class="badge badge-outline font-medium text-[10px]">${el}</span>`);
+    return htmlElements.join(" "); 
+};
+// ------------------------
+//  <div class="">${createElements(issue.labels)}</div>
+// -------------------
+
+
+// Display Cards
+
+function displayIssues(issues){
+
+container.innerHTML = ""
+
+issues.forEach(issue => {
+
+const borderColor =
+issue.status === "open"
+? "border-green-500"
+: "border-purple-500"
+
+
+const card = document.createElement("div")
+
+card.className = `card bg-white shadow border-t-4 ${borderColor}`
+
+
+card.innerHTML = `
+
+<div class="card-body">
+
+<div class="flex justify-between items-center">
+                <span
+                    class="badge badge-outline text-[green] font-medium text-[10px]">${issue.status}</span>
+                <span
+                    class="badge badge-outline text-[#EF4444] font-medium text-[10px]">${issue.priority}</span>
+            </div>
+
+            <h2 class="card-title cursor-pointer font-semibold text-[14px]"
+                onclick="showDetails(${issue.id})">${issue.title}</h2>
+            <p id="description" class="text-[#64748B] text-[12px]">${issue.description}</p>
+
+            <div class="">
+
+                <div class="flex items-center gap-1">${createElements(issue.labels)}</div>
+
+                
+            </div>
+            <div class="divider"></div>
+
+            <p><span class="text-[12px]">#1
+                    by &nbsp</span>${issue.author}</p>
+            <p class="text-[12px]">${issue.createdAt}</p>
+        </div>
+`;
+
+container.appendChild(card)
+
+})
+
+}
+
+
+// Modal Details
+
+async function showDetails(id){
+
+spinner.classList.remove("hidden")
+const res = await fetch(
+`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+
+const data = await res.json()
+
+const issue = data.data
+console.log(issue);
+
+// --------------------------------------------------
+// document.getElementById("modalTitle").textContent = issue.title
+// document.getElementById("modalStatus").textContent = issue.status
+// document.getElementById("modalAuthor").textContent = issue.author
+// document.getElementById("modalCreatedAt").textContent = issue.createdAt
+// document.getElementById("modalAssignee").textContent = issue.assignee
+// document.getElementById("modalLabels").textContent = issue.labels
+// document.getElementById("modalDescription").textContent = issue.description
+// document.getElementById("modalPriority").textContent = issue.priority
+// document.getElementById("issueModal").showModal()
+// ------------------------------------------------------------
+// spinner.classList.add("hidden")
+
+const detailsModal = document.getElementById("details-modal")
+detailsModal.innerHTML = `
+<h2 id="modalTitle" class="font-bold text-[24px]">${issue.title}</h2>
+
+            <div class="flex items-center gap-2">
+                <span id="modalStatus"
+                    class="badge badge-outline text-white text-[blue] font-medium text-[10px]">${issue.status}</span>
+
+                <p class="text-[#64748B] text-[12px]">.&nbsp Opened by &nbsp<span id="modalAuthor">${issue.author}</span>
+                </p>
+                <p class="text-[#64748B] text-[12px]">.&nbsp<span id="modalCreatedAt">${issue.createdAt}</span></p>
+            </div>
+
+            <div id="modalLabels" class="flex items-center gap-2 py-3 badge font-medium text-[10px]">
+
+                <div class="flex items-center gap-1">${createElements(issue.labels)}</div>
+            </div>
+
+            <p id="modalDescription" class="text-[#64748B]">${issue.description}</p>
+            <div class="flex items-center gap-[150px] bg-gray-100 p-2 rounded-xl">
+                <p>Assignee:<br><span id="modalAssignee" class="font-semibold">${issue.assignee ? issue.assignee : "Assignee not found"}</span></p>
+
+                <p>Priority:<br><span class="badge badge-outline text-[#EF4444] font-medium text-[10px]"
+                        id="modalPriority">${issue.priority}</span></p>
+            </div>
+
+`;
+
+// ${word.word ? word.word : "word পাওয়া যায়নি"}
+
+document.getElementById("issueModal").showModal()
+spinner.classList.add("hidden")
+
+
+}
+
+
+// Search
+
+async function searchIssue(){
+
+const text = document.getElementById("searchInput").value
+spinner.classList.remove("hidden")
+const res = await fetch(
+`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${text}`
+)
+
+const data = await res.json()
+
+displayIssues(data.data)
+spinner.classList.add("hidden")
+
+}
