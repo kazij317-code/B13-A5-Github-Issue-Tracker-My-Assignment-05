c04ce1200c22a4e6d9072b562a8e8222661811c0
kazij317-code <kazij317@gmail.com>
2026-03-08 13:36:01 +0600

    load issue and display card functionn is created

--- script_app.js
+++ script_app.js
@@ -23,3 +23,105 @@ function countIssues(issues){
     document.getElementById("openCount").innerText = open;
     document.getElementById("closedCount").innerText = closed;
 }
+// ---------------------------------------------------------------------------------------
+
+// Load All Issues:
+
+const API = "https://phi-lab-server.vercel.app/api/v1/lab/issues"
+
+const container = document.getElementById("issuesContainer")
+const spinner = document.getElementById("spinner")
+
+
+// Load Issues
+
+async function loadIssues(type){
+
+spinner.classList.remove("hidden")
+
+const res = await fetch(API)
+const data = await res.json()
+
+let issues = data.data
+
+
+if(type === "open"){
+issues = issues.filter(issue => issue.status === "open")
+}
+
+if(type === "closed"){
+issues = issues.filter(issue => issue.status === "closed")
+}
+
+countIssues(issues);
+displayIssues(issues)
+
+spinner.classList.add("hidden")
+
+}
+
+loadIssues("all")
+
+
+const createElements = (arr) =>{
+    const htmlElements = arr.map((el) => `<span class="badge badge-outline font-medium text-[10px] bg-[#D97706]">${el}</span>`);
+    return htmlElements.join(" "); 
+};
+// ------------------------
+//  <div class="">${createElements(issue.labels)}</div>
+// -------------------
+
+// Display Cards
+
+function displayIssues(issues){
+
+container.innerHTML = ""
+
+issues.forEach(issue => {
+
+const borderColor =
+issue.status === "open"
+? "border-green-500"
+: "border-purple-500"
+
+
+const card = document.createElement("div")
+
+card.className = `card bg-white shadow border-t-4 ${borderColor}`
+
+
+card.innerHTML = `
+
+<div class="card-body">
+
+<div class="flex justify-between items-center">
+                <span
+                    class="badge badge-outline text-[green] font-medium text-[10px]">${issue.status}</span>
+                <span
+                    class="badge badge-outline text-[#EF4444] font-medium text-[10px]">${issue.priority}</span>
+            </div>
+
+            <h2 class="card-title cursor-pointer font-semibold text-[14px]"
+                onclick="showDetails(${issue.id})">${issue.title}</h2>
+            <p id="description" class="text-[#64748B] text-[12px]">${issue.description}</p>
+
+            <div class="">
+
+                <div class="flex items-center gap-1">${createElements(issue.labels)}</div>
+
+                
+            </div>
+            <div class="divider"></div>
+
+            <p><span class="text-[12px]">#1
+                    by &nbsp</span>${issue.author}</p>
+            <p class="text-[12px]">${issue.createdAt}</p>
+        </div>
+`;
+
+container.appendChild(card)
+
+})
+
+}
+// ----------------------------------------------------------------------------------------------------------
